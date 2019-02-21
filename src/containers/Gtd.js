bd8b33b18f3207a90b7dc54abaa0fdbaeb4e347e
Dinuka Arseculeratne <dinukaarseculeratne@Dinukas-MacBook-Pro.local>
2019-02-21 11:17:38 +1100

    cleaned up create-react-app generated code and added containers and components folders for the structure of the application

---
+++ src/containers/Gtd.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Gtd extends Component{
+     render() {
+        return  (
+             <div>
+                 GTD
+             </div>
+         )
+     }
+}
+
+export default Gtd;
