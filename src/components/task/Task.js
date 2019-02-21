1d4430a47cde65af023d79731fb24415c3fb5d4e
Dinuka Arseculeratne <dinukaarseculeratne@Dinukas-MacBook-Pro.local>
2019-02-21 15:14:20 +1100

    add task and update state

--- src/components/task/Task.js
+++ src/components/task/Task.js
@@ -9,7 +9,7 @@ const task = (props) =>{
     return (
       <div className={classes.Task}>
           <label>Task</label>
-          <input type="text" />
+          <input type="text" ref={props.passRef}/>
           <TaskActions taskActions={taskActionsRequired}/>
       </div>
     );
--- src/containers/Gtd.js
+++ src/containers/Gtd.js
@@ -1,27 +1,39 @@
 import React, {Component} from 'react';
 import Task from '../components/task/Task';
 import TaskList from '../components/tasklist/TaskList';
+
 class Gtd extends Component {
 
     state = {
-        tasks: [
+        tasks: []
+    }
 
-        ]
+    constructor(props) {
+        super(props);
+        this.inputRef = React.createRef();
     }
 
     taskRemovedHandler = (taskName) => {
-        console.log('Task removed for : '+ taskName);
+        console.log('Task removed for : ' + taskName);
     };
 
     taskAddedHandler = () => {
-        console.log('Task added');
+        const addedTask = this.inputRef.current.value;
+        let updatedTasks = Array(...this.state.tasks);
+        updatedTasks.push({
+            name: addedTask,
+            date: new Date().toLocaleDateString("en-US")
+        });
+        this.setState({
+            tasks: updatedTasks
+        })
     };
 
     render() {
         return (
             <div>
-                <Task taskAdded={this.taskAddedHandler}/>
-                <TaskList taskRemoved={this.taskRemovedHandler} tasks={this.state.tasks} />
+                <Task taskAdded={this.taskAddedHandler} passRef={this.inputRef}/>
+                <TaskList taskRemoved={this.taskRemovedHandler} tasks={this.state.tasks}/>
             </div>
         )
     }
